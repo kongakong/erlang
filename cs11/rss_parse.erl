fb901f0db41cdc7cb433e662dda83391398a405a
reader20 <anthony.hw.kong@gmail.com>
2013-05-09 19:07:23 +1000

    Intermediate version

--- cs11/rss_parse.erl
+++ cs11/rss_parse.erl
@@ -1,7 +1,7 @@
 -module(rss_parse).
 
--export([is_rss2_feed/1]).
--export([test/0, test1/1, test2/1]).
+-export([is_rss2_feed/1, compare_feed_items/2]).
+-export([test/0, test1/1, test2/1, test3/1]).
 
 %% -include does not work for library
 %% otherwise it will give 'cannot find file' error
@@ -14,8 +14,7 @@
 %% 2.
 %%
 %% caller will use this in this manner
-%% {XML, RemainingText} = xmerl_scan:file(Filename).
-%%
+%% > {XML, RemainingText} = xmerl_scan:file(Filename).
 %% > is_rss2_feed(XML).
 %% [#xmlText{parents = [{rss,1}],
 %%           pos = 1,language = [],value = "\n ",type = text},
@@ -37,18 +36,62 @@ get_item_time(Item) ->
     ND = httpd_util:convert_request_date(T#xmlText.value),
     calendar:datetime_to_gregorian_seconds(ND).
 
+%% 5
+%% 
+% @private
+% @doc Given an XML element of some kind, this helper function will go through
+%      and remove any details about other XML elements, for example in the
+%      "parents" or "pos" fields.
+%
+% @spec extract_xml(Node::xmlAny()) -> xmlAny()
+%
+extract_xml(Elem = #xmlElement{}) ->
+    Elem#xmlElement{parents=[], pos=0,
+        content=lists:map(fun extract_xml/1, Elem#xmlElement.content),
+        attributes=lists:map(fun extract_xml/1, Elem#xmlElement.attributes)};
+extract_xml(Attr = #xmlAttribute{}) ->
+    Attr#xmlAttribute{parents=[], pos=0};
+extract_xml(Text = #xmlText{}) ->
+    Text#xmlText{parents=[], pos=0};
+extract_xml(Comment = #xmlComment{}) ->
+    Comment#xmlComment{parents=[], pos=0};
+extract_xml(Other) ->
+    Other.
+
+compare_feed_items(OldItem, NewItem) ->
+    OldItem2 = extract_xml(OldItem),
+    NewItem2 = extract_xml(NewItem),
+    io:format("LHS: ~p~n", [OldItem2]),
+    io:format("RHS: ~p~n", [NewItem2]),
+    compare_feed_normalised_items(OldItem2, NewItem2).
+
+compare_feed_normalised_items(OldItem, NewItem) 
+      when OldItem#xmlElement.name =:= NewItem#xmlElement.name -> 
+	      same;
+compare_feed_normalised_items(OldItem, NewItem) ->
+    updated;
+compare_feed_normalised_items(OldItem, NewItem) ->
+    different.
+
+
+
+
 %% Helper function for quick testing
 %% 
 test() ->
-    test2("digg-science-rss1.xml").
+    test3("digg-science-rss1.xml").
 
 test1(F) ->
     {XML, _} = xmerl_scan:file(F),
     is_rss2_feed(XML).
 
 test2(F) ->
-    F = "digg-science-rss1.xml",
     {XML, _} = xmerl_scan:file(F),
     % get_feed_items(XML).
     map(fun get_item_time/1, get_feed_items(XML)).
+
+test3(F) ->
+    {XML, _} = xmerl_scan:file(F),
+    [X1, X2| _] = get_feed_items(XML),
+    compare_feed_items(X1, X2).
 
